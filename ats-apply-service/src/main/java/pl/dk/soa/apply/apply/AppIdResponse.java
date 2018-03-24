f651d4de5de50a9a25f969d0454554d2a9660e05
dkublik <dkublik@gmail.com>
2018-03-25 00:30:25 +0100

    initial

---
+++ ats-apply-service/src/main/java/pl/dk/soa/apply/apply/AppIdResponse.java
@@ -0,0 +1,14 @@
+package pl.dk.soa.apply.apply;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppIdResponse {
+    private String applicationId;
+}
