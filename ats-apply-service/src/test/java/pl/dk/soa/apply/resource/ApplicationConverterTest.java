6e4d967925ed8a5d82cab49b5fa03cfe871ce5f1
dkublik <dkublik@gmail.com>
2018-03-25 13:41:52 +0200

    unit test for application converter

---
+++ ats-apply-service/src/test/java/pl/dk/soa/apply/resource/ApplicationConverterTest.java
@@ -0,0 +1,30 @@
+package pl.dk.soa.apply.resource;
+
+import org.junit.jupiter.api.Test;
+import pl.dk.soa.apply.store.StoredApplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationConverterTest {
+
+    ApplicationConverter applicationConverter = new ApplicationConverter();
+
+    @Test
+    void shouldConvertApplication() {
+        // given
+        Application application = new Application();
+        application.setCandidateId("jSnow");
+        application.setListingId("123");
+        application.setMessageToRecruiter("eager to work for the whole winter!");
+
+        // when
+        StoredApplication storedApplication = applicationConverter.toStoredApplication(application);
+
+        // then
+        assertNotNull(storedApplication.getId());
+        assertEquals("jSnow", storedApplication.getCandidateId());
+        assertEquals("123", storedApplication.getListingId());
+        assertEquals("eager to work for the whole winter!", storedApplication.getMessageToRecruiter());
+    }
+
+}
