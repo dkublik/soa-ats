e7a1341c3460c2a9d8d11e6d89c94d8cddadcd62
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-14 13:18:37 +0200

    restTemplate working with Prefill in stub mode

--- ats-apply-service/src/main/java/pl/dk/soa/apply/notification/MqNotifier.java
+++ ats-apply-service/src/main/java/pl/dk/soa/apply/notification/MqNotifier.java
@@ -1,10 +1,15 @@
 package pl.dk.soa.apply.notification;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 import pl.dk.soa.apply.store.StoredApplication;
+
+import java.io.IOException;
 
 import static pl.dk.soa.apply.notification.JmsConfig.DESTINATION_NAME;
 
@@ -22,11 +27,23 @@ public class MqNotifier {
         this.jmsTemplate = jmsTemplate;
         this.restTemplate = restTemplate;
         this.prefillService = prefillServiceHost + PREFILL_ENDPOINT;
+        restTemplateShouldWorkInTheStubMode();
     }
 
     public void sendNotificationAboutStoredApp(StoredApplication application) {
         Prefill prefillData = restTemplate.getForObject(prefillService, Prefill.class, application.getCandidateId());
         jmsTemplate.convertAndSend(DESTINATION_NAME, new Notification(application, prefillData));
+    }
+
+    private void restTemplateShouldWorkInTheStubMode() {
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                if (response.getStatusCode().value() == 501) {
+                    return false;
+                }
+                return super.hasError(response);
+            }
+        });
     }
 
 }
