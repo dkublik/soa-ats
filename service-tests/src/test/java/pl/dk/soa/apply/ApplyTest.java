c817767eb38badd8cf68cca6b0781c7c526cf3a3
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-16 22:16:19 +0200

    ApplyTest-end2end-wiremock-rule-excercise

--- service-tests/src/test/java/pl/dk/soa/apply/ApplyTest.java
+++ service-tests/src/test/java/pl/dk/soa/apply/ApplyTest.java
@@ -1,10 +1,12 @@
 package pl.dk.soa.apply;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.CoreMatchers;
 import org.json.JSONObject;
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
@@ -18,6 +20,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
  *  ats-prefill-service/src/main/java/pl.dk.soa.prefill.PrefillApplication http://localhost:8081
  */
 public class ApplyTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8081);
 
     @Test
     public void shouldApplySuccessfully() throws Exception {
@@ -39,7 +44,25 @@ public class ApplyTest {
                 .statusCode(HttpStatus.ACCEPTED.value())
                 .contentType(ContentType.JSON)
                 .body("applicationId", notNullValue())
-                .body("priority", CoreMatchers.is("LOW"));
+                .body("priority", CoreMatchers.is("HIGH"));
     }
+
+    /* zadanie:
+        w pliku /src/test/resources/mappings umiesc scenariusz wiremock -> getPrefill.json,
+        ktory dla uzytkownika z testu powyzej (just_britney)
+        zwroci conajmniej:
+        - adress email,
+        - wiecej niz 10 lat doswiadczenia
+
+        skladnie wiremock mozesz podejrzec w plikach, ktore byly przykladami w cwiczeniu z samochodem, np.
+        https://github.com/dkublik/soa-car-template/blob/master/mocks/src/main/resources/mappings/_gps-coordinates.json
+
+        dokladne nazwy parametrow w odpowiedzi i adres mozesz znalezc w swagerze dla prefill-service
+        http://localhost:8081/swagger-ui.html
+        (do wejscia na url /swagger-ui.htm -> prefill-serwis musi byc odpalony,
+        do poprawnego dzialania testu - zastopowany)
+        */
+
+
 
 }
