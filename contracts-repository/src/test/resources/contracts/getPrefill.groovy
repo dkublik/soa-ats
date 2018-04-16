8d75eeb57108fdf467a94365c3473f6cd47232a1
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2018-04-17 00:01:50 +0200

    external-contract-repository

---
+++ contracts-repository/src/test/resources/contracts/getPrefill.groovy
@@ -0,0 +1,20 @@
+
+
+org.springframework.cloud.contract.spec.Contract.make {
+    request {
+        method 'GET'
+        url '/v1/prefill/for-candidate/just_britney'
+    }
+    response {
+        status 200
+        body([
+                "firstName": "Britney",
+                "lastName": "Spears",
+                "email": "just_britney@spears.pl",
+                "yearOfExperience": 15
+        ])
+        headers {
+            contentType('application/json')
+        }
+    }
+}
--- service-tests/src/test/java/pl/dk/soa/apply/ApplyTest.java
+++ service-tests/src/test/java/pl/dk/soa/apply/ApplyTest.java
@@ -6,12 +6,7 @@ import io.restassured.specification.RequestSpecification;
 import org.hamcrest.CoreMatchers;
 import org.json.JSONObject;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -21,9 +16,6 @@ import static org.hamcrest.CoreMatchers.notNullValue;
  *  must be up & running
  *  ats-apply-service/src/main/java/pl.dk.soa.apply.ApplyApplication http://localhost:8080
  */
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@AutoConfigureStubRunner(ids = "pl.dk:prefill-service:+:stubs:8081", stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 public class ApplyTest {
 
     @Test
